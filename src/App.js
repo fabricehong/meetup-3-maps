edfc324f70c681bcef90b06d03c344c08d49cdbf
Julien <jemonet@gmail.com>
2018-07-24 19:51:49 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Text>Hello</Text>
+      </div>
+    )
+  }
+}
