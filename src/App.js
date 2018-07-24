73e36d9d99ae87b07a373b63f80248190c5fa4af
Julien <jemonet@gmail.com>
2018-07-24 20:18:00 +0200

    First correction ... tested

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <View>
         <Text>Hello</Text>
-      </div>
+      </View>
     )
   }
 }
